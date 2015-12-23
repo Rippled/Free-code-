9e7e1e6fba6ad543b369120ab768956b6a2a7109
Rippled <notforsale@gmx.com>
2015-12-23 01:21:40 +0000

    Create datediff.php

---
+++ datediff.php
@@ -0,0 +1,9 @@
+<?php
+//This function accepts the date in mysql format i.e 2016-01-01
+function datediff($date1,$date2) {
+			$date1 = date_create($date1);
+			$date2 = date_create($date2);
+			$diff = date_diff($date1,$date2);
+			return $diff->format("%a");
+}
+?>
